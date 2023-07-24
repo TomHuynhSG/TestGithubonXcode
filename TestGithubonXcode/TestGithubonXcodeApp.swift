63b0ba1bf9fbbd184768f6c11499bffc8d1628ec
Tom Huynh <tomhuynhsg@gmail.com>
2023-07-24 10:46:18 +0700

    Initial Commit

---
+++ TestGithubonXcode/TestGithubonXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGithubonXcodeApp.swift
+//  TestGithubonXcode
+//
+//  Created by Tom Huynh on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestGithubonXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
